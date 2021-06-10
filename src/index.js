b70e9fa7da1f70c7f989e2cdc111757af3fcf8bb
Kate <kamennaya.kate@gmail.com>
2021-06-10 17:39:36 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -2,61 +2,63 @@ import './sass/main.scss';
 import PixabayApiService from './js/apiService.js';
 import debounce from 'lodash.debounce';
 import cardsTpl from './templates/cards.hbs'
+import LoadMoreBtn from './js/loadMoreBtn';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   imagesContainer: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+  //loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
+
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
 
 const pixabayApiService = new PixabayApiService();
 
 refs.searchForm.addEventListener('input', debounce(onSearch, 500));
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.refs.button.addEventListener('click', fetchImages);
 
 function onSearch(event) {
-  console.log(event);
+  const valueInput = event.target.value;
 
-  
+  if (valueInput.trim() === '') {
+    return alert('Неверный запрос!');
+  }
+
   pixabayApiService.query = event.target.value;
+
+  loadMoreBtn.show();
   pixabayApiService.resetPage();
-  pixabayApiService.fetchImages().then(images => {
-    clearImagesContainer();
-    renderCards(images);
-    });  
+  clearImagesContainer();
+  fetchImages();
 
 }
 
-function onLoadMore() {
- pixabayApiService.fetchImages().then(renderCards);    
+function fetchImages() {
+  loadMoreBtn.disable();
+
+  pixabayApiService.fetchImages()
+    .then(images => {
+      renderCards(images);
+      loadMoreBtn.enable()
+    }
+    );
 }
 
-// function renderCards(images) {
-//   const markup = cardsTpl(images);
-//   const elements = refs.imagesContainer.querySelectorAll('.photo-card');
-//   //console.log(elements);
-//   const element = elements[elements.length-1];
-//   refs.imagesContainer.insertAdjacentHTML('beforeend', markup);
-
-//   if (element) {
-//   element.scrollIntoView({
-//   behavior: 'smooth',
-//   block: 'end',
-// })};
-// }
 
 function renderCards(images) {
-//   const markup = cardsTpl(images);
-//   const elements = refs.imagesContainer.querySelectorAll('.photo-card');
-//   //console.log(elements);
-//   const element = elements[elements.length-1];
-   refs.imagesContainer.insertAdjacentHTML('beforeend', cardsTpl(images));
-
-//   if (element) {
-//   element.scrollIntoView({
-//   behavior: 'smooth',
-//   block: 'end',
-// })};
+  const markup = cardsTpl(images);
+  refs.imagesContainer.insertAdjacentHTML('beforeend', markup);
+  //const elements = refs.imagesContainer.querySelectorAll('.photo-card');
+  //const element = elements[elements.length - 1];
+  //if (element) {
+  loadMoreBtn.refs.button.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  })
+  // };
 }
 
 function clearImagesContainer() {
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -3,23 +3,16 @@ const BASE_URL = 'https://pixabay.com/api';
 
 export default class PixabayApiService {
   constructor() {
-    this.searchQuery = '';
+    this.searchQuery ='';
     this.page = 1;
   }
 
   fetchImages() {
-    //console.log(this);
-    // const options = {
-    //   headers: {
-    //       Authorization: API_KEY,
-    //   },
-    // };
     const url = `${BASE_URL}/?key=${API_KEY}&image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12`;
 
    return fetch(url)
    .then(response => response.json())
    .then(data => {
-     //console.log(data);
      this.page +=1;
      return data.hits;
    });
@@ -44,15 +37,3 @@ export default class PixabayApiService {
 
 
 
-// fetchImages() {
-//   const url = `${BASE_URL}/?key=${API_KEY}&image_type=photo&orientation=horizontal&q=flowers&page=1&per_page=12`;
-
-//     return fetch(url, options)
-//       .then(response => response.json())
-//       .then(({ images }) => {
-//         console.log(images);
-//         //this.incrementPage();
-//         return images;
-//       });
-//   }
-// }
